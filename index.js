40971d6d738b733cc8feb94f3b38aa2d75dd09d1
Travis Fowlston <travisfowlston@ymail.com>
2023-11-26 19:59:08 -0800

    started working in index.js file

--- index.js
+++ index.js
@@ -2,4 +2,25 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-console.log(hiiiiiii)
+const questions = [
+  {
+    type: "input",
+    name: "projectName",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "How would you describe your project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please include your installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please include your usage information:",
+  },
+];
